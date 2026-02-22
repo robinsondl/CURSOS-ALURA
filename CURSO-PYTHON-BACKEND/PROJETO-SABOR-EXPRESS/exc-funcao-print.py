99eed5eed5c5db60101df9aa608d33767ed24893
robinsondl <robinsondahrog00@gmail.com>
2026-02-21 22:23:24 -0300

    Primeira Aula - Criação do APP e Exc de Print
    Primeira Aula: Criação do APP e Exc de Print
    - Iniciar a criação um app para realizar pedidos em um restaurante;
    - Utilizar Print(), Input(), Variáveis, quebra de linhas, chamar a Variável dentro da Print() de forma rápida.

---
+++ CURSO-PYTHON-BACKEND/PROJETO-SABOR-EXPRESS/exc-funcao-print.py
@@ -0,0 +1,24 @@
+#Link para a Letra: https://fsymbols.com/
+
+print("""
+𝓟𝓨𝓣𝓗𝓞𝓝 𝓝𝓐 𝓔𝓢𝓒𝓞𝓛𝓐 𝓓𝓔 𝓟𝓡𝓞𝓖𝓡𝓐𝓜𝓐𝓒̧𝓐̃𝓞 𝓐𝓛𝓤𝓡𝓐
+""");
+
+nome = input("Qual o seu nome: ");
+idade = input("Quantos anos você tem: ")
+
+print(f"""\nObrigado por se apresentar! Agora sei que seu nome é {nome} e você tem {idade} anos.Seja bem vindo!""")
+
+#print("""
+#A
+#L
+#U
+#R
+#A
+#""")
+
+print("\nA","L","U","R","A\n", sep= "\n")
+
+pi_arredondado = 3.14159
+
+print(f"O valor de pi é {pi_arredondado:.2f}!")
